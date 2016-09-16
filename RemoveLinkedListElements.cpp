483404a780bba7e9f8e1d12523fb93d9c48be10d
zyearn <zhujiashun2010@gmail.com>
2016-09-16 19:25:14 +0800

    update

---
+++ RemoveLinkedListElements.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        while (head && head->val == val) {
+            head = head->next;
+        }
+
+        ListNode *ret = head;
+        while (head && head->next) {
+            if (head->next->val == val) {
+                head->next = head->next->next;
+            } else {
+                head = head->next;
+            }
+        }
+
+        return ret;
+    }
+};
