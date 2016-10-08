99f02fb16ac23168209537b3afd3269bcd15d54e
zyearn <zhujiashun2010@gmail.com>
2016-10-08 14:01:59 +0800

    Floyd

---
+++ hiho/1089.cpp
@@ -0,0 +1,167 @@
+#include <map>
+#include <vector>
+#include <stack>
+#include <set>
+#include <queue>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <climits>
+using namespace std;
+
+#define mpr make_pair
+typedef unsigned int ui;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <double, double> pdd;
+typedef vector <int> vi;
+typedef vector <ll> vll;
+typedef vector <double> vd;
+typedef vector <string> vs;
+typedef map <string, int> mpsi;
+typedef map <double, int> mpdi;
+typedef map <int, int> mpii;
+
+
+double pi = acos(0.0) * 2.0;
+long double eps = 1e-10;
+
+inline bool isAlpha(char a) {
+    return a >= 'A' && a<= 'Z';
+}
+
+inline bool isZero(double x) {
+    if (x < eps && x > -eps) {
+        return true;
+    }
+
+    return false;
+}
+
+template <class T> inline T abs1(T a) {return a < 0 ? -a : a;}
+
+template <class T> inline T max1(T a, T b) { return b < a ? a : b; }
+template <class T> inline T max1(T a, T b, T c) { return max1(max1(a, b), c); }
+template <class T> inline T max1(T a, T b, T c, T d) { return max1(max1(a, b, c), d); }
+template <class T> inline T max1(T a, T b, T c, T d, T e) { return max1(max1(a, b, c, d), e); }
+template <class T> inline T min1(T a, T b) { return a < b ? a : b; }
+template <class T> inline T min1(T a, T b, T c) { return min1(min1(a, b), c); }
+template <class T> inline T min1(T a, T b, T c, T d) { return min1(min1(a, b, c), d); }
+template <class T> inline T min1(T a, T b, T c, T d, T e) { return min1(min1(a, b, c, d), e); }
+
+long long pow(long long n, long long m, long long mod = 0){
+	if(m < 0) return 0;
+	long long ans = 1 % mod;
+	long long k = n;
+
+	while(m){
+		if(m & 1) {
+			ans *= k;
+			if(mod) ans %= mod;
+		}
+		k *= k;
+		if(mod) k %= mod;
+		m >>= 1;
+	}
+
+	return ans;
+}
+
+/**
+ * 
+ * read int: scanf("%d\n", &t);
+ * read str: fgets(buf, sizeof(buf), stdin);
+ * 迭代器用auto
+ * printf("%lld", t)
+ *
+ */
+
+int N, E, s, e;
+
+int edges[101][101];
+
+inline void swap(int &a, int &b) {
+    int tmp = a;
+    a = b;
+    b = tmp;
+    return;
+}
+
+int main() {
+    scanf("%d %d\n", &N, &E);
+    int a, b, w;
+    map<int, int> m;
+
+    for (int i=0; i<101; i++) {
+        for (int j=0; j<101; j++) {
+            
+            if (i == j) {
+                edges[i][j] = 0;
+            } else {
+                edges[i][j] = INT_MAX;
+            }
+        }
+    }
+
+    for (int i=0; i<E; i++) {
+        scanf("%d %d %d\n", &a, &b, &w);
+
+        if (b > a) {
+            swap(a, b);
+        }
+
+        int num = a * 10000 + b;
+        if (m.find(num) == m.end()) {
+            m[num] = w;
+        } else {
+            if (w < m[num]) {
+                m[num] = w;
+            }
+        }
+    }
+
+    for (auto it=m.begin(); it!=m.end(); it++) {
+        int a = it->first / 10000;
+        int b = it->first % 10000;
+        int w = it->second;
+            
+        edges[a][b] = w;
+        edges[b][a] = w;
+    }
+
+    for (int k=1; k<=N; k++) {
+        for (int i=1; i<=N; i++) {
+            for (int j=1; j<=N; j++) {
+                if (i == k || i == j || k == j) {
+                    continue;
+                }
+
+                if (edges[i][k] == INT_MAX || edges[k][j] == INT_MAX) {
+                    continue;
+                }
+
+                edges[i][j] = min(edges[i][j], edges[i][k] + edges[k][j]);
+            }
+        }
+    }
+
+    for (int i=1; i<=N; i++) {
+        for (int j=1; j<=N; j++) {
+            if (j == 1) {
+                cout << edges[i][j];
+            } else {
+                cout << " " <<edges[i][j];
+            }
+        }
+
+        cout << endl;
+    }
+
+
+    return 0;
+}
